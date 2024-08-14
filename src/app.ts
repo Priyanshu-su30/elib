a21bf38abc3461aff3d8bc14ae4e5dd160c16a3f
Priyanshu <priyanshusingh.0601@gmai.com>
2024-08-14 12:54:52 +0530

    add register user validation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import globalErrorHandler from './middleware/globalErrorHandler';
 import userRouter from './user/userRoutes';
 
 const app = express();
+app.use(express.json());
+
 // Routes
 // Http methods GET, POST, PUT, PATCH, DELETE
 app.get("/", (req, res, next) => {
