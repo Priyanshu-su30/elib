9efaf83d2dae0243e6f81c783d7d0367d2452579
Priyanshu <priyanshusingh.0601@gmai.com>
2024-08-15 16:30:12 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModels from "./userModels";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const {name, email, password} = req.body;
+    
     //Validation
     if(!name || !email || !password){
         const error = createHttpError(400, "All fields are required");
@@ -18,10 +23,24 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(error);
     }
 
-    //process
-    //Response
+    //password->hash
+    const hashedPassword = await bcrypt.hash(password,10);
 
-    res.json({message: "User Created"});
+    const newUser = await userModels.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    //Token generation JWT
+    const token = sign({sub: newUser._id}, config.jwtSecret as string, {
+        expiresIn: '7d',
+        algorithm: "HS256",
+    });
+    
+    //Response
+    
+    res.json({ accessToken: token});
 
 };
 
