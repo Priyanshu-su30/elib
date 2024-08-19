d80b7fc023462c00bec23fa4fadf7618b651956b
Priyanshu <priyanshusingh.0601@gmai.com>
2024-08-19 18:07:28 +0530

    add author id to new book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -5,6 +5,7 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
+import { AuthRequest } from "../middleware/authenticate";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     const {title, genre} = req.body;
@@ -39,15 +40,13 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         console.log("bookFileUploadResult", bookFileUploadResult);
         
     
-        console.log('uploadResult', uploadResult);
-        //@ts-ignore
-        console.log('userId', req.userId);
+        const _req = req as AuthRequest;
         
         
         const newBook = await bookModel.create({
             title,
             genre,
-            author: "66bddcc6e4bfd15a63ed5f46",
+            author: _req.userId,
             coverImage: uploadResult.secure_url,
             file: bookFileUploadResult.secure_url,
         });
